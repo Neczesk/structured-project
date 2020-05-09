5a9ecd94b4e3cf55c62af1df0196e41deda63b0a
kyle <haltermann.kyle@gmail.com>
2020-05-09 13:28:18 -0700

    Loading and saving implemented basics

--- project.go
+++ project.go
@@ -1,21 +1,25 @@
 package project
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
 )
 
 //Project contains metadata about the project, and the sceneTree which holds all project scenes
 type Project struct {
-	author    string
-	title     string
-	sceneTree sceneTree
+	Author    string
+	Title     string
+	SceneTree sceneTree
 }
 
 //NewBlankProject creates a new project with as many blank, zero valued initials as possible
-func NewBlankProject() (Project, error) {
+func NewBlankProject() Project {
 	scenetree := newBlankSceneTree()
 	p := Project{"", "", scenetree}
-	return p, nil
+	return p
 }
 
 /*
@@ -27,3 +31,52 @@ func (p *Project) summarize() string {
 	wordCount := countWords(p.SceneTree.Root)
 	return fmt.Sprintf("This project has %d scenes, %d of which are leaves, with a wordcount of %d\n", sceneCount, leafCount, wordCount)
 }
+
+//SaveProject saves a project to the filename given in the parameters in the running user's Documents directory.
+func (p *Project) SaveProject(filename string) error {
+	saveData, err := json.Marshal(p)
+	cwd, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+	savepath := path.Join(cwd, "Documents")
+	savepath = path.Join(savepath, filename)
+	return ioutil.WriteFile(savepath, []byte(saveData), 0600)
+}
+
+func loadProject(data []byte) (Project, error) {
+	loadedProject := Project{}
+	json.Unmarshal(data, &loadedProject)
+	return loadedProject, nil
+}
+
+//LoadProjectFile takes a file name, returns a Project object
+func LoadProjectFile(filename string) (Project, error) {
+	d, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return NewBlankProject(), err
+	}
+	return loadProject(d)
+}
+
+// func FullyLoadProject(filename string) error {
+// 	d, err := ioutil.ReadFile(filename)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	loadedProject := Project{}
+// 	json.Unmarshal(d, &loadedProject)
+// 	fmt.Println(loadedProject.summarize())
+// 	p = &loadedProject
+// 	return nil
+// }
+
+// //getScene takes a sceneid string and returns the scene object it refers to.
+// func (p *Project) getScene(id string) scene {
+
+// }
+
+// //UpdateScene takes a sceneid string and a scene object, and overwrites the scene with that id with the scene parameter
+// func (p *Project) UpdateScene(id string, scenedata scene) {
+
+// }
--- project_test.go
+++ project_test.go
@@ -2,22 +2,31 @@ package project
 
 import (
 	"fmt"
+	"io/ioutil"
 	"testing"
 )
 
 func TestProject(t *testing.T) {
-	p, err := NewBlankProject()
-	if err != nil {
-		t.Errorf("Error creating project")
-	}
+	p := NewBlankProject()
 	s := p.SceneTree.newScene("Hi mom", "Hello mother")
 	p.SceneTree.AddScene("root", &s)
 	if p.SceneTree.countScenes() != 1 {
-		t.Errorf("Expected %d scenes, found %d", p.SceneTree.CountScenes(), 1)
+		t.Errorf("Expected %d scenes, found %d", p.SceneTree.countScenes(), 1)
 	}
 	s1 := p.SceneTree.newScene("I am a new scene", "Hell")
 	p.SceneTree.AddScene(s.ID, &s1)
 	s2 := p.SceneTree.newScene("I am a leaf", "leaf leaf leaf")
 	p.SceneTree.AddScene(s.ID, &s2)
-	fmt.Print(p.summarize())
+	err := p.SaveProject("toast.project")
+	d, err := ioutil.ReadFile("/Users/kyle/Documents/toast.project")
+	if err != nil {
+		t.Errorf("Error reading file")
+	}
+	newProject, err := loadProject(d)
+	if p.summarize() != newProject.summarize() {
+		t.Errorf("Saved and loaded project not the same as original")
+	}
+	x := NewBlankProject()
+	x, err = LoadProjectFile("/Users/kyle/Documents/toast.project")
+	fmt.Println(x.summarize())
 }
